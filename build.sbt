caa437185745f050d6b4c854c786675272cad1a8
anjanbachhu <anjan.bachhu@gmail.com>
2016-11-29 18:12:29 +0000

    Initial commit - default project structure

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "ShopperProject"
+
+version := "1.0"
+
+scalaVersion := "2.12.0"
+    
---
+++ src/main/scala/com/hmrc/checkout/Main.scala
@@ -0,0 +1,7 @@
+package com.hmrc.checkout
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println(s"Checkout started")
+  }
+}
